8ac5f92fdf6358b7c8bf4dbed5baaf72205735b3
Jichao Ouyang <oyanglulu@gmail.com>
2021-10-20 17:55:56 +1100

    move bin to lambda extensions default directory

--- docker.nix
+++ docker.nix
@@ -1,11 +1,15 @@
 let pkgs = import ./nixpkgs.nix;
     binary = pkgs.haskell.lib.justStaticExecutables (import ./default.nix {});
+    extensionAlias = pkgs.runCommand "symlink" {} ''
+      mkdir -p $out/opt/extensions
+      ln -s ${binary}/bin/aws-lambda-oauth-extension-exe $out/opt/extensions/aws-lambda-oauth-extension
+      '';
 in pkgs.dockerTools.streamLayeredImage {
   name = "ghcr.io/myob-oss/aws-lambda-oauth-extension";
   tag = "latest";
   created = "now";
-  contents = [binary pkgs.cacert];
+  contents = [binary pkgs.cacert extensionAlias];
   config = {
-    Entrypoint = [ "/bin/aws-lambda-oauth-extension-exe"];
+    Entrypoint = [ "/opt/extensions/aws-lambda-oauth-extension"];
   };
 }
