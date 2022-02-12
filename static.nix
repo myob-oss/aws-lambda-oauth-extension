97e96ccff8bec60dae320a9abf2be93a9453a7e7
Jichao Ouyang <oyanglulu@gmail.com>
2022-02-12 23:36:39 +1100

    disable time-compat test

--- static.nix
+++ static.nix
@@ -1,7 +1,20 @@
 with (import ./nixpkgs.nix);
 
 haskell.lib.appendConfigureFlags
-  (haskell.lib.justStaticExecutables (pkgs.haskell.lib.dontCheck (import ./default.nix {pkgs = pkgsMusl;}).aws-lambda-oauth-extension-cabal))
+  (haskell.lib.justStaticExecutables (pkgs.haskell.lib.dontCheck (import ./default.nix {
+    pkgs = pkgsMusl;
+    haskellPackage = pkgsMusl.haskell.packages.ghc8107.override (old: {
+      overrides = let
+                extension =
+                  haskellPackagesNew: haskellPackagesOld: {
+                    time-compat = pkgsMusl.haskell.lib.dontCheck haskellPackagesOld.time-compat;
+                  };
+              in
+                pkgsMusl.lib.composeExtensions
+                  (old.overrides or (_: _: {}))
+                  extension;
+    });
+  }).aws-lambda-oauth-extension-cabal))
   [ "--enable-executable-static"
     "--extra-lib-dirs=${pkgsMusl.ncurses.override { enableStatic = true; enableShared = true; }}/lib"
     "--extra-lib-dirs=${pkgsMusl.gmp6.override { withStatic = true; }}/lib"
